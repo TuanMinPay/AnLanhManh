62235d9b03d2b95c7197927175ef6a2be302545d
Ngo Van Tuan <contact.tuannv@gmail.com>
2019-11-01 13:50:11 +0700

    create project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { AboutComponent } from './about/about.component';
+import { ContacusComponent } from './contacus/contacus.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'product/:id', component: ProductComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContacusComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
