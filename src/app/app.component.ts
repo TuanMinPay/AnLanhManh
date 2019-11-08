5d2c1a44704b08099c0ace52207865913eae81c5
Ngo Van Tuan <contact.tuannv@gmail.com>
2019-11-08 23:05:25 +0700

    add ngOnInit and logout to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AnLanhManh';
+export class AppComponent implements OnInit  {
+
+  constructor() { }
+
+  isLogin: boolean = false;
+
+  logout() {
+    // handle logout
+  }
+
+  ngOnInit() {
+    var token = localStorage.getItem('token');
+    if (token == null || token == undefined || token == '') {
+      this.isLogin = false;
+    } else {
+      this.isLogin = true;
+    }
+  }
 }
