e9315abe755fe0bf50c0f5037a8bfb632c2b10f7
NguyenDanhVuong <vuongndd00547@fpt.edu.vn>
2019-11-08 21:20:30 +0700

    validate-register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import axios from "axios";
 import {Router} from "@angular/router"
-import { FormBuilder, FormGroup, Validators, ControlContainer } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 
 @Component({
@@ -12,60 +12,49 @@ import { FormBuilder, FormGroup, Validators, ControlContainer } from '@angular/f
 export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup;
-
+  submitted = false;
+  
   constructor(
     private router: Router,
-    private formBuilder: FormBuilder,
-   ) {
-     
-   }
+    private formBuilder: FormBuilder) { }
 
 
-
-  ngOnInit() {
+  ngOnInit(){
     this.registerForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      username: ['', Validators.required],
-      email: ['', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      phone: ['',  Validators.required]
+      name: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(40)]],
+      username: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(100)]],
+      phone: ['', [Validators.required, Validators.maxLength(20)]]
     }); 
-  }
-
-  
-
-  get f() { return this.registerForm.controls; }
+  } 
 
   public handleRegister: Function = async (name: any, username: any, email: any, password: any, phone: any) => {
-
     const that = this;
+    that.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    }
     await axios.post('http://localhost:8080/api/auth/signup', {
       name: name,
       username: username,
       email: email,
       password: password,
       phone: phone
-    }).then(function (response) {
-      console.log(response);
-      if(response.data.status == 201){
-          that.router.navigate(['/login'])
-      }
-    }).catch(function (error) {
-      // if(error instanceof )
-      if(error.response.data.status == 400){
-        console.log(error.response.data.errors);
-        document.getElementById("errorName").innerHTML = error.response.data.errors;
-      }
+      }).then(function (response) {
+        console.log(response);
+        if(response.data.status == 201){
+            that.router.navigate(['/login'])
+        }
+      }).catch(function (error) {
+      // if(error.response.status == 400){
+      //   this.errorMessage = error.response.message;
+      // }
+
+      console.log(error.response);
       
 
-      // document.getElementById("errorUsername").innerHTML = error.response.data;
-
-      // document.getElementById("errorEmail").innerHTML = error.response.data;
-
-      // document.getElementById("errorPassword").innerHTML = error.response.data;
-
-      // document.getElementById("errorPhone").innerHTML = error.response.data;
-        
+      
     });
   }
 }
