bb35e2694791d87bd2132e66f7de39cf4237a2dd
Ngo Van Tuan <contact.tuannv@gmail.com>
2019-11-09 17:18:15 +0700

    fix login fail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'
-import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,32 +8,23 @@ import { from } from 'rxjs';
 export class AppComponent implements OnInit  {
 
   constructor(
-    private router: Router,
   ) { }
 
   isLogin: boolean = false;
 
-  
   logout() {
-    const that = this;
     var token = localStorage.getItem('token');
-    if (token != null || token != undefined || token != ''){
+    if (token != null || token != undefined){
       this.isLogin = true;
       localStorage.removeItem('token');
-      window.location.href = '/login';
       this.isLogin = false;
-      // if(localStorage.getItem('token') == null){
-      //   alert("null");
-      // }
-      // console.log();
-      //that.router.navigate(['/login'])
+      window.location.href = window.location.href;
     }
   }
 
   ngOnInit() {
     var token = localStorage.getItem('token');
-    console.log(token);
-    if (token == null || token == undefined || token == '') {
+    if (token == null || token == undefined) {
       this.isLogin = false;
     } else {
       this.isLogin = true;
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import axios from "axios";
-import {Router} from "@angular/router"
+import { Router } from "@angular/router"
 
 @Component({
   selector: 'app-login',
@@ -11,45 +11,39 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private router: Router,
-   ) {}
+  ) { }
+
+  textError: any = null;
+  public handleLogin: Function = async (account: any, password: any) => {
+    const that = this;
+    if (password.length == 0 || account.length == 0) {
+      that.textError = "Please enter full infomation !";
+    } else {
+      that.textError = null;
+      await axios.post('http://localhost:8080/api/auth/signin', {
+        account: account,
+        password: password
+      }).then(function (response) {
+        if (response.data.status == 200) {
+          localStorage.setItem("token", response.data.accessToken);
+          window.location.href = '/';
+        }
+      }).catch(function (error) {
+        if (error.response.data.status == 401) {
+          that.textError = error.response.data.message;
+        } else {
+          that.textError = null;
+        }
+      });
+    }
+  }
 
   ngOnInit() {
-    const that = this;
-    var token = localStorage.getItem('token');
-    console.log(token);
-    if (token != null || token != undefined || token != ''){
+    var token: any = localStorage.getItem('token');
+    if (token != null || token != undefined) {
       window.location.href = '/';
     }
   }
 
-  forgot = true;
-  forgot2 = true;
-  public handleLogin: Function = async (account: any, password: any) => {
-    const that = this;
-    if(password.length == 0 || account.length == 0 ){
-      that.forgot = false;
-    }else{
-      that.forgot = true;
-      await axios.post('http://localhost:8080/api/auth/signin', {
-      account: account,
-      password: password
-    }).then(function (response) {
-      //success
-      if(response.data.status == 200){
-        localStorage.setItem("token", response.data.accessToken);
-        //console.log(localStorage.getItem("token"));
-        window.location.href = '/';
-        //that.router.navigate(['/'])
-      }
-    }).catch(function (error) {
-      //error
-      console.log(error.response.data);
-      if(error.response.data.status == 401){
-        document.getElementById("errorResponse").innerHTML = error.response.data.message;
-      }
-      that.forgot2 = false;
-    });
-    }
-  }
 }
 
