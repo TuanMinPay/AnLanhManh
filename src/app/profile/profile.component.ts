7aeafef3f46e57e144aefe3aa56f28e14e035d02
Hungpvhd <hungpvhd00529@fpt.edu.vn>
2019-11-11 01:02:12 +0700

    Get set detail

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { async } from '@angular/core/testing';
+import axios from 'axios';
 
 @Component({
   selector: 'app-profile',
@@ -10,11 +12,30 @@ export class ProfileComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.getProfile();
+  }
+
+  userDetails: any;
+  id: any;
+  API_PROFILE = 'http://localhost:8080/api/user-profile' + this.id;
+
+  public getProfile: Function = async => {
+    const that = this;
+    axios.get(that.API_PROFILE)
+    .then(function (response){
+      that.userDetails = response.data.data;
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
   }
 
   logout() {
     var token = localStorage.getItem('token');
+    if (token != null || token != undefined){
       localStorage.removeItem('token');
       window.location.href = '/login';
-  }
+    }
+  } 
 }
--- src/app/set/set.component.ts
+++ src/app/set/set.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import axios from 'axios'
+import { async } from 'q';
 @Component({
   selector: 'app-set',
   templateUrl: './set.component.html',
@@ -9,7 +10,24 @@ export class SetComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  listProduct: any = [];
+
+  API_COMBO = "http://localhost:8080/api/combo/1";
+
+  public getSet: Function = async => {
+    const that = this;
+    axios.get(that.API_COMBO)
+    .then(function (response) {
+      console.log(response);
+      that.listProduct = response.data.data.foodSet;
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
   }
 
+  ngOnInit() {
+    this.getSet();
+  }
 }
