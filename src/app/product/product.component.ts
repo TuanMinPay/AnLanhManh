2330336a134a99f26baa2724062e58edcff4de06
NguyenDanhVuong <vuongndd00547@fpt.edu.vn>
2019-11-15 03:18:44 +0700

    product-pagination

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import axios from "axios";
+import * as _ from 'underscore';
+import { parseLazyRoute } from '@angular/compiler/src/aot/lazy_routes';
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
- 
-  dataFood: any =[{
+
+  dataFood: any = [{
     id: null,
     name: null,
     description: null,
@@ -34,8 +37,8 @@ export class ProductComponent implements OnInit {
     private router: Router,
     private route: ActivatedRoute
   ) {
-   }
-  
+  }
+
   pager: any = [{
     limit: null,
     page: null,
@@ -43,36 +46,68 @@ export class ProductComponent implements OnInit {
     totalPages: null
   }];
 
-  pageOfItems = [];
-  
-  page : number;
-  
-  ngOnInit() {
-    const that = this;
-    that.page = that.route.snapshot.params['page'];
-    that.route.queryParams.subscribe(x => that.loadPage(x.page || 1));
-    
-  }  
+  pageOfItems: any = [];
+  currentPage: number = 1;
+  page: any;
+  pages: any = [];
+  startPage: number; endPage: number;
 
-  private loadPage(page: any){
-    const that = this;
-    axios.get<any>('http://localhost:8080/api/food/' + that.page)
-    .then(function (response) {
-      if(response.data.status == 200){
-        that.dataFood = response.data.data.content;
-        that.pager = response.data.restPagination;
-        // that.pageOfItems = response.data.restPagination
-      }
-      console.log(response.data.data.content);
-      console.log(response.data.restPagination);
-    })
-    .catch(function (error: any) {
-      // handle error
-      console.log(error);
-    });
+  @ViewChild("target", { static: false }) target: ElementRef;
+
+  setPage(page: number) {
+    this.currentPage = page;
+    this.router.navigate( ['/product/food/list'],  { queryParams: { page: page } });
+    this.loadPage(page);
+    this.target.nativeElement.scrollIntoView({ block: 'start',  behavior: 'smooth', inline: 'nearest' });
   }
 
-   foodDetail(id: number){
+  ngOnInit() {
+    this.page = this.route.snapshot.queryParamMap.get('page');
+    if (this.page == null) {
+      this.page = 1;
+    } else {
+      this.page = parseInt(this.page);
+    }
+    this.currentPage = this.page;
+    this.loadPage(this.page);
+  }
+
+  private loadPage(page: number) {
+    const that = this;
+    axios.get('http://localhost:8080/api/food/list?page=' + page)
+      .then(function (response) {
+        if (response.data.status == 200) {
+          that.dataFood = response.data.data;
+          that.pager = response.data.restPagination;
+          that.pageOfItems = Math.ceil(response.data.restPagination.totalItems / response.data.restPagination.limit);
+          if (that.pageOfItems <= 10) {
+            that.startPage = 1;
+            that.endPage = that.pageOfItems;
+          } else {
+            if (that.currentPage <= 6) {
+              that.startPage = 1;
+              that.endPage = 10;
+            } else if (that.currentPage + 4 >= that.pageOfItems) {
+              that.startPage = that.pageOfItems - 9;
+              that.endPage = that.pageOfItems;
+            } else {
+              that.startPage = that.currentPage - 5;
+              that.endPage = that.currentPage + 4;
+            }
+          }
+          that.pages = _.range(that.startPage, that.endPage + 1);
+        }
+
+        console.log(response.data.data);
+        console.log(response.data.restPagination);
+      })
+      .catch(function (error: any) {
+        // handle error
+        console.log(error);
+      });
+  }
+
+  foodDetail(id: number) {
     this.router.navigate(["/product-detail/food", id]);
   }
 }
