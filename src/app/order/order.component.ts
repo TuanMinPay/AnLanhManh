ad7e23de891bf5944f3dde42ab5543a25833b448
Hungpvhd <hungpvhd00529@fpt.edu.vn>
2019-11-30 00:55:13 +0700

    gke

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { AppComponent } from "../app.component"
+import { UtilService } from '../services/util.service';
 
 @Component({
   selector: 'app-order',
@@ -7,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public util: UtilService
+  ) { }
+
+  listCart: any = null;
+
+  getTotalPriceCart() {
+    return this.util.formatPrice(this.listCart.products.reduce((a, b) => parseInt(a) + parseInt(b.price), 0));
+  }
+
+  getProduct() {
+    const that = this;
+    that.listCart = localStorage.getItem('listCart');
+    if (that.listCart != null || that.listCart != undefined) {
+      that.listCart = localStorage.getItem('listCart');
+      that.listCart = JSON.parse(that.listCart);
+      console.log(that.listCart);
+    } else {
+      localStorage.removeItem('listCart');
+      that.listCart == null;
+      console.log(that.listCart);
+    }
+  }
 
   ngOnInit() {
-    
+    this.getProduct();
+
   }
 
 }
