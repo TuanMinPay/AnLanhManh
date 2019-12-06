4f04759488273e3650f1dd4f420249d08844b7ef
Hungpvhd <hungpvhd00529@fpt.edu.vn>
2019-12-06 18:32:07 +0700

    create ate form

---
+++ src/app/ate/ate.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
+import { environment } from '../../environments/environment';
+
+@Component({
+  selector: 'app-ate',
+  templateUrl: './ate.component.html',
+  styleUrls: ['./ate.component.css']
+})
+export class AteComponent implements OnInit {
+
+  constructor() { }
+
+  listFood: any [];
+
+  isShowForm = false;
+
+  getFood(){
+    const that = this;
+    axios.get(`${environment.api_url}/api/combo`)
+    .then(function (response){
+      console.log(response);
+      that.listFood = response.data.data;
+    })
+    .catch(function (error){
+      console.log(error);
+      
+    })
+  }
+
+  showForm(){
+    const that = this;
+    that.isShowForm = true;
+  }
+
+  back(){
+    this.isShowForm = false;
+  }
+
+  ngOnInit() {
+    this.getFood();
+  }
+
+}
